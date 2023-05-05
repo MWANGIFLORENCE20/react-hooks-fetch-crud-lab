509240a48da5a9ef6e0e7f03b97585fc1f850678
florence wangechi <florencewangechim@gmail.com>
2023-05-05 23:54:31 +0300

    Completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const[questions,setQuestions]=useState()
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then((r)=>(
+       console.table(r),
+       r.json(r)
+    ))
+    .then((data)=>(
+      console.log(data),
+      setQuestions(data)
+    ))
+  },[])
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <QuestionItem/>
     </section>
   );
 }
